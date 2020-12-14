8adb59ff1570e2ecd8f417ab83e8a71d73238a7d
nievescanas <nievescanasmartin@gmail.com>
2020-12-14 15:31:57 +0100

    Cration of component Removal_objects

---
+++ components/removal_objects.js
@@ -0,0 +1,24 @@
+// Component: Removal of objects
+// Input elements: id(obj)
+// Ej: removal_object="event: click; id:box"
+
+AFRAME.registerComponent('removal_object', {
+  // Is an object that defines and describes the property
+  schema: {
+     id: {type: 'string'},                  // Object name
+     parse: AFRAME.utils.styleParser.parse
+  },
+  init: function () {
+    const data = this.data;
+
+    el.addEventListener(data.event, evt => {
+      var obj = document.getElementById(data.id);
+      if (!obj){
+    		alert("The selected item does not exist");
+    	} else {
+    		padre = obj.parentNode; // Get the parent node of the element
+    		padre.removeChild(obj);
+    	}
+    });
+  }
+});
